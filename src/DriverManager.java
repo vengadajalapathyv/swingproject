f533c57ffe4b82bb7387ed17f7f13d5a8404d016
vengadajalapathyv <vengad3105@gmail.com>
2024-05-30 18:50:28 +0530

    Add files via upload

---
+++ src/DriverManager.java
@@ -0,0 +1,19 @@
+
+import com.sun.jdi.connect.spi.Connection;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author venga
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcsqlserverDESKTOP0N2GOP3SQLEXPRESSencr, String nirmal, String nirmal0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
